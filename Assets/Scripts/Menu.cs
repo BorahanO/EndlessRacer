d66ac71930b772d49b5a8423fc82b2f238bb102c
mrSmellyfeet100 <boraoxal@gmail.com>
2017-07-10 15:01:19 +0200

    Initial Commit

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+
+	// Use this for initialization
+	public void PlayGame (int level)
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void ReturnMenu()
+    {
+        Application.LoadLevel(0);
+    }
+}
+
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+	public float speed;
+
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+
+		GetComponent<Rigidbody>().AddForce (movement * speed * Time.deltaTime);
+
+		{
+			if (Input.GetKeyDown ("space") && GetComponent<Rigidbody>().transform.position.y <= 0.6250001f) {
+				Vector3 jump = new Vector3 (0.0f, 200.0f, 0.0f);
+
+				GetComponent<Rigidbody>().AddForce (jump);
+			}
+		}
+
+	}
+
+
+}
